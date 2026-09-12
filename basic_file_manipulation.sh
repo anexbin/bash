caa62c795477f85f77f939be03b97c133512dda6
AnesX <your-email@example.com>
2026-09-12 12:43:00 +0100

    adding basic file manipulation commands

---
+++ basic_file_manipulation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "this is a bash file "
+# This is how to rename files in bash
+# be carefull you can overwrite files if you mistaken the name
+mv lesson1.txt lesson2.txt
+# This is how to remove files in bash
+rm lesson2.txt
+# This is how we create files in bash
+touch lesson3.txt lesson4.txt lesson5.txt lesson6.txt
+# This is how we print files in the working directory
+ls
+# This is how we remove all lesson files using wildcard *
+rm lesson*.txt
+# This is how we clear the terminal screen
+clear
+# This is how we show the history of commands we typed
+history
